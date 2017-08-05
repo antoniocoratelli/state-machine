7b5ff0e810f0fae659fd1850871b30ceb11dfffd
Antonio Coratelli <antoniocoratelli@gmail.com>
2017-08-05 13:53:19 +0200

    Add test cases and comments

--- examples/OnOffSwitch/OnOffSwitch.cpp
+++ examples/OnOffSwitch/OnOffSwitch.cpp
@@ -4,21 +4,34 @@
 TEST(TestOnOffSwitch, main) {
     using namespace antoniocoratelli;
 
+    // Initializing the switch: it's Off by default.
     OnOffSwitch on_off_switch;
-
     std::cout << on_off_switch.info() << std::endl;
     EXPECT_EQ(true, on_off_switch.in<StateOff>());
     EXPECT_EQ(false, on_off_switch.in<StateOn>());
 
+    // This simple switch doesn't have non-controllable events in any state,
+    // thus any call to the update() method will have no effect.
     on_off_switch.update();
-
     std::cout << on_off_switch.info() << std::endl;
     EXPECT_EQ(true, on_off_switch.in<StateOff>());
     EXPECT_EQ(false, on_off_switch.in<StateOn>());
 
+    // Turning on the switch, we expect that the state changes to On
     on_off_switch.trigger<EventTurnOn>();
-
     std::cout << on_off_switch.info() << std::endl;
     EXPECT_EQ(false, on_off_switch.in<StateOff>());
     EXPECT_EQ(true, on_off_switch.in<StateOn>());
+
+    // Trying to trigger an event that can't be triggered in this state
+    EXPECT_THROW(on_off_switch.trigger<EventTurnOn>(), std::logic_error);
+
+    // Turning off the switch, we expect that the state changes to Off
+    on_off_switch.trigger<EventTurnOff>();
+    std::cout << on_off_switch.info() << std::endl;
+    EXPECT_EQ(true, on_off_switch.in<StateOff>());
+    EXPECT_EQ(false, on_off_switch.in<StateOn>());
+
+    // Trying to trigger an event that can't be triggered in this state
+    EXPECT_THROW(on_off_switch.trigger<EventTurnOff>(), std::logic_error);
 }
