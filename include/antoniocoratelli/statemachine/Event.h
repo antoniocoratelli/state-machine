65c3a046b06664e26b964a8d417b78a10c4a0aae
Antonio Coratelli <antoniocoratelli@gmail.com>
2017-08-05 21:05:41 +0200

    Add documentation for State and Event

--- include/antoniocoratelli/statemachine/Event.h
+++ include/antoniocoratelli/statemachine/Event.h
@@ -5,6 +5,11 @@
 namespace antoniocoratelli {
 namespace statemachine {
 
+/**
+ * Base interface for all types of Events.
+ * @see ControllableEvent
+ * @see NonControllableEvent
+ */
 class Event {
 public:
     virtual Transition execute() = 0;
@@ -12,11 +17,19 @@ public:
     virtual ~Event() {}
 };
 
+/**
+ * Base class for all the events that can be controlled by the user
+ * of the state machine.
+ */
 class ControllableEvent: public Event {
 public:
     virtual ~ControllableEvent() {}
 };
 
+/**
+ * Base class for all the events that are out of the control of the
+ * user of the state machine. For example: timers, failures.
+ */
 class NonControllableEvent: public Event {
 public:
     virtual ~NonControllableEvent() {}
--- include/antoniocoratelli/statemachine/State.h
+++ include/antoniocoratelli/statemachine/State.h
@@ -12,26 +12,65 @@ namespace statemachine {
 class ControllableEvent;
 class NonControllableEvent;
 
+/**
+ * State defines the interface for implementing all the states of a given StateMachine.
+ * Each concrete state must register the ControllableEvents that can be triggered when the
+ * state is active, calling `State::attach<ControllableEvent>()` in the constructor.
+ * For example:
+ * @code
+ *   class StateIncomingCall: public State {
+ *   public:
+ *     StateIncomingCall() {
+ *       this->attach<EventAnswer>();
+ *       this->attach<EventIgnore>();
+ *     }
+ *   }
+ * @endcode
+ * will enable the ControllableEvents "Answer" and "Ignore" when the system is in state
+ * "IncomingCall".
+ */
 class State {
 public:
     typedef std::unique_ptr<State> uptr_t;
 
-    State() {}
+    /**
+     * Must be implemented by each concrete state to define the logic for handling
+     * NonControllable Events.
+     * If the state is designed not to have any NonControllable Events, the function
+     * can be simply defined as: `return {}`.
+     */
+    virtual Transition update() = 0;
+
+    /**
+     * Short string representation of the state.
+     */
+    virtual std::string name() = 0;
+
+    /**
+     * Extended string representation of the state.
+     */
+    virtual std::string info() = 0;
+
+    /**
+     * Returns true iff the state can trigger an event of type `controllable_event_t`,
+     * that is, iff the event `controllable_event_t` was attached to the state.
+     */
+    template<class controllable_event_t> bool triggers() const;
 
     virtual ~State() {}
 
-    virtual Transition update() = 0;
-
-    virtual std::string name() = 0;
-
-    virtual std::string info() = 0;
-
-    template<class controllable_event_t> bool triggers() const;
-
 protected:
-   template<class controllable_event_t> void attach();
+    /**
+     * Registers events of type `controllable_event_t` as a ControllableEvent
+     * that can be triggered when the StateMachine is in the state defined
+     * by this object.
+     */
+    template<class controllable_event_t> void attach();
 
 private:
+    /**
+     * Set of the ControllableEvents that were attached to this state.
+     */
     std::set<std::type_index> _allowed_controllable_events;
 };
 
