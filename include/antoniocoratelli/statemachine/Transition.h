a5ad6b4572535cbb7f707f9f809946cd1f0bebed
Antonio Coratelli <antoniocoratelli@gmail.com>
2017-08-05 18:41:24 +0200

    Add documentation for Transition.h

--- include/antoniocoratelli/statemachine/Transition.h
+++ include/antoniocoratelli/statemachine/Transition.h
@@ -6,6 +6,13 @@ namespace statemachine {
 
 class State;
 
+/**
+ * Type of object that defines an optional state transition.
+ * When the StateMachine receives a Transition object, the current state
+ * is changed only if the Transition optional has a value.
+ * @attention The ownership of the pointer is assumed to be relased to the
+ *            receiver, which must handle its deallocation.
+ */
 typedef std::experimental::optional<State*> Transition;
 
 } // statemachine
